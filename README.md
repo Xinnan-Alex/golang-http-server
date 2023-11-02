c38f86982cc2eedc2d5c5a8bcce25a5b71e0c519
Xinnan-Alex <leongxinnan@outlook.my>
2023-11-02 21:12:19 +0800

    authentication, rss scraping, post rss data as posts into db

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# golang-http-server
+# Http Server built with Golang
+
+## Tools used:
+- SQLC `go install github.com/sqlc-dev/sqlc/cmd/sqlc@latest`
+- GOOSE `go install github.com/pressly/goose/v3/cmd/goose@latest`
+
+Goose Commands (Local PostgreSQL)
+
+Run UP Migration: <br>
+`goose postgres postgres://<username>:<password>@localhost:5432/<db_name> up`
+
+Run down Migration: <br>
+`goose postgres postgres://<username>:<password>@localhost:5432/<db_name> down`
--- main.go
+++ main.go
@@ -1,13 +1,22 @@
 package main
 
 import (
-	"github.com/go-chi/chi"
-	"github.com/go-chi/cors"
-	"github.com/joho/godotenv"
+	"database/sql"
 	"log"
 	"net/http"
 	"os"
+	"time"
+
+	"github.com/Xinnan-Alex/rssagg/internal/database"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
+
+type apiConfig struct {
+	DB *database.Queries
+}
 
 func main() {
 
@@ -20,6 +29,23 @@ func main() {
 	if portString == "" {
 		log.Fatal("PORT is not found in the environment")
 	}
+
+	dbURL := os.Getenv("DB_URL")
+	if dbURL == "" {
+		log.Fatal("DB_URL is not found in the environment")
+	}
+
+	conn, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	db := database.New(conn)
+	apiCfg := apiConfig{
+		DB: db,
+	}
+
+	go startScraping(db, 10, time.Minute)
 
 	router := chi.NewRouter()
 
@@ -35,6 +61,18 @@ func main() {
 	v1Router := chi.NewRouter()
 	v1Router.Get("/healthz", handlerReadiness)
 	v1Router.Get("/err", handlerErr)
+	// Router for users
+	v1Router.Post("/users", apiCfg.handlerCreateUser)
+	v1Router.Get("/users", apiCfg.middlewareAuth(apiCfg.handlerGetUser))
+	//Router for feeds
+	v1Router.Post("/feeds", apiCfg.middlewareAuth(apiCfg.handlerCreateFeed))
+	v1Router.Get("/feeds", apiCfg.HandlerGetFeeds)
+	//Router for feed follows
+	v1Router.Post("/feed_follows", apiCfg.middlewareAuth(apiCfg.handlerCreateFeedFollow))
+	v1Router.Get("/feed_follows", apiCfg.middlewareAuth(apiCfg.handlerGetFeedFollows))
+	v1Router.Delete("/feed_follows/{feedFollowID}", apiCfg.middlewareAuth(apiCfg.HandlerDeleteFeedFollow))
+	v1Router.Get("/posts", apiCfg.middlewareAuth(apiCfg.handlerGetPostsForUser))
+
 	router.Mount("/v1", v1Router)
 
 	srv := &http.Server{
